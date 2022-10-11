406ba17f72fca3b2dc15836db69d405c4c3119f4
Hasanuzzaman92 <hasanuzzamanmethu@gmail.com>
2022-10-11 14:44:27 +0600

    Blog pages added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -2,9 +2,120 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-           <h3>This is a blog page</h3> 
+        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="grid max-w-2xl mx-auto">
+        <div className="flex">
+          <div className="flex flex-col items-center mr-6">
+            <div className="w-px h-10 opacity-0 sm:h-full" />
+            <div>
+              <div className="flex items-center justify-center w-8 h-8 text-xs font-medium border rounded-full">
+                1
+              </div>
+            </div>
+            <div className="w-px h-full bg-gray-300" />
+          </div>
+          <div className="flex flex-col pb-6 sm:items-center sm:flex-row sm:pb-0">
+            <div className="sm:mr-5">
+              <div className="flex items-center justify-center w-16 h-16 my-3 rounded-full bg-indigo-50 sm:w-24 sm:h-24">
+                <svg
+                  className="w-12 h-12 text-deep-purple-accent-400 sm:w-16 sm:h-16"
+                  stroke="currentColor"
+                  viewBox="0 0 52 52"
+                >
+                  <polygon
+                    strokeWidth="3"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    fill="none"
+                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div>
+              <p className="text-xl font-semibold sm:text-base">
+                What is the purpose of react-router?
+              </p>
+              <p className="text-sm text-gray-700">
+              React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL.
+              </p>
+            </div>
+          </div>
         </div>
+        <div className="flex">
+          <div className="flex flex-col items-center mr-6">
+            <div className="w-px h-10 bg-gray-300 sm:h-full" />
+            <div>
+              <div className="flex items-center justify-center w-8 h-8 text-xs font-medium border rounded-full">
+                2
+              </div>
+            </div>
+            <div className="w-px h-full bg-gray-300" />
+          </div>
+          <div className="flex flex-col pb-6 sm:items-center sm:flex-row sm:pb-0">
+            <div className="sm:mr-5">
+              <div className="flex items-center justify-center w-16 h-16 my-3 rounded-full bg-indigo-50 sm:w-24 sm:h-24">
+                <svg
+                  className="w-12 h-12 text-deep-purple-accent-400 sm:w-16 sm:h-16"
+                  stroke="currentColor"
+                  viewBox="0 0 52 52"
+                >
+                  <polygon
+                    strokeWidth="3"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    fill="none"
+                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div>
+              <p className="text-xl font-semibold sm:text-base mt-5">How does the context-api work?</p>
+              <p className="text-sm text-gray-700">
+              The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex">
+          <div className="flex flex-col items-center mr-6">
+            <div className="w-px h-10 bg-gray-300 sm:h-full" />
+            <div>
+              <div className="flex items-center justify-center w-8 h-8 text-xs font-medium border rounded-full">
+                3
+              </div>
+            </div>
+            <div className="w-px h-full opacity-0" />
+          </div>
+          <div className="flex flex-col pb-6 sm:items-center sm:flex-row sm:pb-0">
+            <div className="sm:mr-5">
+              <div className="flex items-center justify-center w-16 h-16 my-3 rounded-full bg-indigo-50 sm:w-24 sm:h-24">
+                <svg
+                  className="w-12 h-12 text-deep-purple-accent-400 sm:w-16 sm:h-16"
+                  stroke="currentColor"
+                  viewBox="0 0 52 52"
+                >
+                  <polygon
+                    strokeWidth="3"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    fill="none"
+                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div>
+              <p className="text-xl font-semibold sm:text-base mt-5">what is useref hook in react?</p>
+              <p className="text-sm text-gray-700">
+              The useRef Hook allows you to persist values between renders. It can be used to store a mutable value that does not cause a re-render when updated. It can be used to access a DOM element directly. useRef returns a mutable ref object whose .current property is initialized to the passed argument ( initialValue ). The returned object will persist for the full lifetime of the component. Essentially, useRef is like a “box” that can hold a mutable value in its .current property.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     );
 };
 
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -10,7 +10,7 @@ const Statistics = () => {
         <div>
             <h2 className='text-2xl font-semibold text-center text-blue-500 py-8'>Quiz topics and total number of quiz</h2>
             <br/>
-            <div className='flex justify-center w-[80%], mx-auto'>
+            <div className='flex justify-center mt-20 lg:my-5'>
             <LineChart height={400} width={500} data={data}>
                 <Line type="monotone" dataKey="total" stroke="#82ca9d" />
                 <XAxis dataKey="name" />
